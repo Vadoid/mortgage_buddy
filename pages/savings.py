3de995413848e07262c3486a2d57ad07cfb2a95b
VadimBureau7 <vadim@bureau7.com>
2024-05-16 19:24:38 +0100

    logo

--- pages/savings.py
+++ pages/savings.py
@@ -8,6 +8,32 @@ locale.setlocale(locale.LC_ALL, '')
 # Set page layout to wide
 st.set_page_config(page_title="Mortgage Buddy", layout="wide")
 
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(https://raw.githubusercontent.com/Vadoid/mortgage_buddy/main/img/buddy.png);
+                background-repeat: no-repeat;
+                background-position: right;
+                padding-top: 60px;
+                background-size: 160px 150px;
+                background-position: 20px 20px;
+            }
+            [data-testid="stSidebarNav"]::before {
+                content: "";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+add_logo()
+
 def calculate_compound_interest(principal, annual_rate, inflation_rate, years):
     data = []
     total_balance = principal
